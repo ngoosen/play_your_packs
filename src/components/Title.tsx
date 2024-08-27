c94ceae86117af28fcfb6e2c84a11a43ace56d05
Noémie Goosen <noemie.goosen@gmail.com>
2024-08-27 13:02:48 +0200

    style: added basic styling to title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,9 @@ import styles from "../styles/components/Title.module.scss";
 export default function Title(): JSX.Element {
   return (
     <div className={styles.main}>
-      <h1>Asaide's Play your packs challenge</h1>
+      <div className={styles.title}>
+        <h1>Asaide's Play your packs challenge</h1>
+      </div>
     </div>
   );
 }
