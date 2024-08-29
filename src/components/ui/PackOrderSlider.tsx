03337fb1f40d5ccf6b4c9ff8539c94822807f186
Noémie Goosen <noemie.goosen@gmail.com>
2024-08-29 14:03:37 +0200

    feat: add packs title to sliders

--- src/components/ui/PackOrderSlider.tsx
+++ src/components/ui/PackOrderSlider.tsx
@@ -31,6 +31,28 @@ function getBackgroundColor(pack: PACKS): string {
   }
 }
 
+function getPackName(pack: PACKS): string {
+  switch (pack) {
+    case 0: return "Au travail";
+    case 1: return "Vivre ensemble";
+    case 2: return "Vie citadine";
+    case 3: return "Chiens et chats";
+    case 4: return "Saison";
+    case 5: return "Heure de gloire";
+    case 6: return "Iles paradisiaques";
+    case 7: return "À la fac";
+    case 8: return "Écologie";
+    case 9: return "Escapade enneigée";
+    case 10: return "Vie à la campagne";
+    case 11: return "Années lycée";
+    case 12: return "Grandir ensemble";
+    case 13: return "Vie au ranch";
+    case 14: return "À louer";
+    case 15: return "Amour fou";
+    default: return "";
+  }
+}
+
 export default function PackOrderSlider(props: IPackOrderSliderProps): JSX.Element {
   const { pack, onReorder, } = props;
 
@@ -44,7 +66,7 @@ export default function PackOrderSlider(props: IPackOrderSliderProps): JSX.Eleme
 
   return (
     <div className={styles.main} style={{ backgroundImage: `url(/img/packs/sliders/${pack}.jpg)`, backgroundColor: getBackgroundColor(pack), }}>
-      <p>{pack}</p>
+      <p className={styles.pack_title}>{getPackName(pack)}</p>
       <div className={styles.sort_arrows}>
         <button onClick={upHandler}>
           <FontAwesomeIcon icon={faCaretUp} className={styles.arrow} />
