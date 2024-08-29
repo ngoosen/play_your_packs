df84b76d30b9b77fa865436902d4fa4703aab4d7
Noémie Goosen <noemie.goosen@gmail.com>
2024-08-29 11:51:20 +0200

    feat: add sliders images

--- src/components/ui/PackOrderSlider.tsx
+++ src/components/ui/PackOrderSlider.tsx
@@ -10,6 +10,27 @@ interface IPackOrderSliderProps {
   onReorder: CallableFunction
 }
 
+function getBackgroundColor(pack: PACKS): string {
+  switch (pack) {
+    case 0:
+    case 1:
+    case 2:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 11:
+    case 14: return "#485ad6";
+    case 3:
+    case 12:
+    case 13:
+    case 15: return "#da448c";
+    case 6:
+    case 9: return "#59b047";
+    default: return "";
+  }
+}
+
 export default function PackOrderSlider(props: IPackOrderSliderProps): JSX.Element {
   const { pack, onReorder, } = props;
 
@@ -22,7 +43,7 @@ export default function PackOrderSlider(props: IPackOrderSliderProps): JSX.Eleme
   }
 
   return (
-    <div className={styles.main}>
+    <div className={styles.main} style={{ backgroundImage: `url(/img/packs/sliders/${pack}.jpg)`, backgroundColor: getBackgroundColor(pack), }}>
       <p>{pack}</p>
       <div className={styles.sort_arrows}>
         <button onClick={upHandler}>
