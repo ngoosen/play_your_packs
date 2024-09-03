0766d73b85845731614fa2ecd886cdfa57276201
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-03 14:30:00 +0200

    style: header's text in html

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,7 +5,20 @@ import Parallax from "./ui/Parallax";
 export default function Title(): JSX.Element {
   return (
     <Parallax img="/img/landing_banner.jpg" className={styles.main}>
-      <img src="/img/title.jpg" alt="" />
+      {/* <img src="/img/title.jpg" alt="" /> */}
+      <div className={styles.title}>
+        <h1>
+          <span id={styles.asaide}>
+            Asaide's
+          </span>
+          <span id={styles.play}>
+            Play your packs
+          </span>
+          <span id={styles.challenge}>
+            challenge
+          </span>
+        </h1>
+      </div>
     </Parallax>
   );
 }
